0ba9cceb27fbeb26903733fe67cd52ccccd780fa
LJH20200323 <2391300825@qq.com>
2021-05-18 10:17:08 +0800

    输入DTO中验证逻辑

--- BookStore/src/Acme.BookStore.Application.Contracts/Users/UserCreationDto.cs
+++ BookStore/src/Acme.BookStore.Application.Contracts/Users/UserCreationDto.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Acme.BookStore.Users
 {
     public class UserCreationDto
     {
+        [Required]
+        [StringLength(UserConsts.MaxUserNameLength)]
         public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(UserConsts.MaxEmailLength)]
         public string Email { get; set; }
+
+        [Required]
+        [StringLength(UserConsts.MaxEmailLength,MinimumLength = UserConsts.MinPasswordLength)]
         public string Password { get; set; }
     }
 }
---
+++ BookStore/src/Acme.BookStore.Domain.Shared/Users/UserConsts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Acme.BookStore.Users
+{
+    public static class UserConsts
+    {
+        public const int MaxUserNameLength = 60;
+
+        public const int MaxEmailLength = 60;
+
+        public const int MinPasswordLength = 32;
+    }
+}
